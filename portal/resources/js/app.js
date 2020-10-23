66f11e9be4457ad88b63ad45565a78c3d9ac1342
ObiPivara <ricardo.affonso@live.com>
2020-10-23 19:21:26 -0400

    indo

--- portal/resources/js/app.js
+++ portal/resources/js/app.js
@@ -30,3 +30,8 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 const app = new Vue({
     el: '#app',
 });
+
+
+function cria_form(){
+console.log('aqui')
+}
--- portal/resources/views/home.blade.php
+++ portal/resources/views/home.blade.php
@@ -6,18 +6,47 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
-                teste
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+                    <div>
+                        <h1>informe as materias</h1>
+                        <div class='div-quantas'>
+                            quantas: <select id="quantas" name='quantas'><option value="1">1</option><option value="2">2</option><option value="3">3</option></select>
+                            <button  class=''  onclick='cria_form();'  value="go">go</button>
                         </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                        
+                        <form  class ='form' action="registra" method="get">
+                            <div class='d-none aqui'></div>
+                            <input type="submit" class='d-none' value="Submit"/>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
+
+
+<div class='linha d-none'>
+    <input name='materia' placeholder='materia'>
+    <select id="tempo" name='tempo'>
+        <option value="2">2h</option>
+        <option value="4">4h</option>
+        <option value="6">6h</option>
+    </select>
+    <select id="dia" name='dia'>
+        <option value="2">segunda</option>
+        <option value="3">terca</option>
+        <option value="4">quarta</option>
+        <option value="5">quinta</option>
+        <option value="6">sexta</option>
+    </select>
+    <input name="_token" type="hidden" value="{{ csrf_token() }}" />
+</div>
+
+
+
+
+
+
+
 @endsection
