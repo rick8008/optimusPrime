6b7fa2fe7663448b8fa3e6fffaf386b50debcdf4
Giovannict42 <giovannict42@live.com>
2020-11-03 18:44:54 -0300

    Novo

--- portal/resources/views/home.blade.php
+++ portal/resources/views/home.blade.php
@@ -79,7 +79,7 @@
 								<div class="input-group-prepend">
 									<span class="input-group-text"><i class="fas fa-user"></i></span>
 								</div>
-								<input name="Nome_Completo" type="text" class="form-control" placeholder="Nome Completo">
+								<input name="Nome_Completo" type="text" class="form-control" placeholder="Nome Completo (ex: Prof. Dr. Lorem Ipsum)">
 							</div>
 							<div class="input-group form-group">
 								<div class="input-group-prepend">
@@ -190,10 +190,118 @@
  <script type="text/javascript">
     hideAll();
  </script>
-
-
-
-
+<br>
+<br>
+<!-- Area de Data
+	data id: segunda; terca; quarta; quinta; sexta; sabado.
+	horario id : 7:30-9:30 = h1; 9:30-11:30 =  h2; 13:30-15:30 = h3; 15:30 - 17:30 = h4 .
+	tipo id: materia = m; nome = n; horario = h;
+	id formatação - data-horario-tipo ex: segunda-h2-materia -->
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+				<div class="card-header"> Tabela de Horarios </div>
+                <div class="table-responsive">
+					<table class="table" style="white-space:nowrap;">
+					<thead class="thead-dark" style="text-align:center">
+						<tr>
+							<th scope="col">#</th>
+							<th scope="col">Segunda-Feira</th>
+							<th scope="col">Terça-Feira</th>
+							<th scope="col">Quarta-Feira</th>
+							<th scope="col">Quinta-Feira</th>
+							<th scope="col">Sexta-Feira</th>
+							<th scope="col">Sabado</th>
+						</tr>
+						</thead>
+					<tbody style="text-align:center">
+						<tr>
+							<th scope="row">7:30 <br> 9:30</th>
+							<td> <p class="font-weight-bold" style="margin:0" id="segunda-h1-m"> ##### </p> <p class="font-weight-light" style="margin:0" id="segunda-h1-n"> Prof. Dr. Lorem Ipsum </p> <p class="font-weight-light" style="margin:0" id="segunda-h1-h"> #h </small> </td>
+							<td> <p class="font-weight-bold" style="margin:0" id="terca-h1-m"> ##### </p> <p class="font-weight-light" style="margin:0" id="terca-h1-n"> Prof. Dr. Lorem Ipsum </p> <p class="font-weight-light" style="margin:0" id="terca-h1-h"> #h </small></td>
+							<td> <p class="font-weight-bold" style="margin:0" id="quarta-h1-m"> ##### </p> <p class="font-weight-light" style="margin:0" id="quarta-h1-n"> Prof. Dr. Lorem Ipsum </p> <p class="font-weight-light" style="margin:0" id="quarta-h1-h"> #h </small></td>
+							<td> <p class="font-weight-bold" style="margin:0" id="quinta-h1-m"> ##### </p> <p class="font-weight-light" style="margin:0" id="quinta-h1-n"> Prof. Dr. Lorem Ipsum </p> <p class="font-weight-light" style="margin:0" id="quinta-h1-h"> #h </small></td>
+							<td> <p class="font-weight-bold" style="margin:0" id="sexta-h1-m"> ##### </p> <p class="font-weight-light" style="margin:0" id="sexta-h1-n"> Prof. Dr. Lorem Ipsum </p> <p class="font-weight-light" style="margin:0" id="sexta-h1-h"> #h </small></td>
+							<td> <p class="font-weight-bold" style="margin:0" id="sabado-h1-m"> ##### </p> <p class="font-weight-light" style="margin:0" id="sabado-h1-n"> Prof. Dr. Lorem Ipsum </p> <p class="font-weight-light" style="margin:0" id="sabado-h1-h"> #h </small></td>
+						</tr>
+						<tr>
+							<th scope="row">9:30 <br> 11:30</th>
+							<td> <p class="font-weight-bold" style="margin:0" id="segunda-h2-m"> ##### </p> <p class="font-weight-light" style="margin:0" id="segunda-h2-n"> Prof. Dr. Lorem Ipsum </p> <p class="font-weight-light" style="margin:0" id="segunda-h2-h"> #h </small> </td>
+							<td> <p class="font-weight-bold" style="margin:0" id="terca-h2-m"> ##### </p> <p class="font-weight-light" style="margin:0" id="terca-h2-n"> Prof. Dr. Lorem Ipsum </p> <p class="font-weight-light" style="margin:0" id="terca-h2-h"> #h </small></td>
+							<td> <p class="font-weight-bold" style="margin:0" id="quarta-h2-m"> ##### </p> <p class="font-weight-light" style="margin:0" id="quarta-h2-n"> Prof. Dr. Lorem Ipsum </p> <p class="font-weight-light" style="margin:0" id="quarta-h2-h"> #h </small></td>
+							<td> <p class="font-weight-bold" style="margin:0" id="quinta-h2-m"> ##### </p> <p class="font-weight-light" style="margin:0" id="quinta-h2-n"> Prof. Dr. Lorem Ipsum </p> <p class="font-weight-light" style="margin:0" id="quinta-h2-h"> #h </small></td>
+							<td> <p class="font-weight-bold" style="margin:0" id="sexta-h2-m"> ##### </p> <p class="font-weight-light" style="margin:0" id="sexta-h2-n"> Prof. Dr. Lorem Ipsum </p> <p class="font-weight-light" style="margin:0" id="sexta-h2-h"> #h </small></td>
+							<td> <p class="font-weight-bold" style="margin:0" id="sabado-h2-m"> ##### </p> <p class="font-weight-light" style="margin:0" id="sabado-h2-n"> Prof. Dr. Lorem Ipsum </p> <p class="font-weight-light" style="margin:0" id="sabado-h2-h"> #h </small></td>
+						</tr>
+						<tr>
+							<th scope="row"></th>
+							<td colspan="6"></td>
+						</tr>
+						<tr>
+							<th scope="row">13:30 <br> 15:30</th>
+							<td> <p class="font-weight-bold" style="margin:0" id="segunda-h3-m"> ##### </p> <p class="font-weight-light" style="margin:0" id="segunda-h3-n"> Prof. Dr. Lorem Ipsum </p> <p class="font-weight-light" style="margin:0" id="segunda-h3-h"> #h </small> </td>
+							<td> <p class="font-weight-bold" style="margin:0" id="terca-h3-m"> ##### </p> <p class="font-weight-light" style="margin:0" id="terca-h3-n"> Prof. Dr. Lorem Ipsum </p> <p class="font-weight-light" style="margin:0" id="terca-h3-h"> #h </small></td>
+							<td> <p class="font-weight-bold" style="margin:0" id="quarta-h3-m"> ##### </p> <p class="font-weight-light" style="margin:0" id="quarta-h3-n"> Prof. Dr. Lorem Ipsum </p> <p class="font-weight-light" style="margin:0" id="quarta-h3-h"> #h </small></td>
+							<td> <p class="font-weight-bold" style="margin:0" id="quinta-h3-m"> ##### </p> <p class="font-weight-light" style="margin:0" id="quinta-h3-n"> Prof. Dr. Lorem Ipsum </p> <p class="font-weight-light" style="margin:0" id="quinta-h3-h"> #h </small></td>
+							<td> <p class="font-weight-bold" style="margin:0" id="sexta-h3-m"> ##### </p> <p class="font-weight-light" style="margin:0" id="sexta-h3-n"> Prof. Dr. Lorem Ipsum </p> <p class="font-weight-light" style="margin:0" id="sexta-h3-h"> #h </small></td>
+							<td> <p class="font-weight-bold" style="margin:0" id="sabado-h3-m"> ##### </p> <p class="font-weight-light" style="margin:0" id="sabado-h3-n"> Prof. Dr. Lorem Ipsum </p> <p class="font-weight-light" style="margin:0" id="sabado-h3-h"> #h </small></td>
+						</tr>
+						<tr>
+							<th scope="row">15:30 <br> 17:30</th>
+							<td> <p class="font-weight-bold" style="margin:0" id="segunda-h4-m"> ##### </p> <p class="font-weight-light" style="margin:0" id="segunda-h4-n"> Prof. Dr. Lorem Ipsum </p> <p class="font-weight-light" style="margin:0" id="segunda-h4-h"> #h </small> </td>
+							<td> <p class="font-weight-bold" style="margin:0" id="terca-h4-m"> ##### </p> <p class="font-weight-light" style="margin:0" id="terca-h4-n"> Prof. Dr. Lorem Ipsum </p> <p class="font-weight-light" style="margin:0" id="terca-h4-h"> #h </small></td>
+							<td> <p class="font-weight-bold" style="margin:0" id="quarta-h4-m"> ##### </p> <p class="font-weight-light" style="margin:0" id="quarta-h4-n"> Prof. Dr. Lorem Ipsum </p> <p class="font-weight-light" style="margin:0" id="quarta-h4-h"> #h </small></td>
+							<td> <p class="font-weight-bold" style="margin:0" id="quinta-h4-m"> ##### </p> <p class="font-weight-light" style="margin:0" id="quinta-h4-n"> Prof. Dr. Lorem Ipsum </p> <p class="font-weight-light" style="margin:0" id="quinta-h4-h"> #h </small></td>
+							<td> <p class="font-weight-bold" style="margin:0" id="sexta-h4-m"> ##### </p> <p class="font-weight-light" style="margin:0" id="sexta-h4-n"> Prof. Dr. Lorem Ipsum </p> <p class="font-weight-light" style="margin:0" id="sexta-h4-h"> #h </small></td>
+							<td> <p class="font-weight-bold" style="margin:0" id="sabado-h4-m"> ##### </p> <p class="font-weight-light" style="margin:0" id="sabado-h4-n"> Prof. Dr. Lorem Ipsum </p> <p class="font-weight-light" style="margin:0" id="sabado-h4-h"> #h </small></td>
+						</tr>
+					</tbody>
+					</table>
+				</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<!--
+matrix(
+		[element[1,1], element[1,2], element[1,3], element[1,4], element[1,5], element[1,6]]
+		[element[2,1], element[2,2], element[2,3], element[2,4], element[2,5], element[2,6]]
+		[element[3,1], element[3,2], element[3,3], element[3,4], element[3,5], element[3,6]]
+		[element[4,1], element[4,2], element[4,3], element[4,4], element[4,5], element[4,6]]
+	)
+-->
+<script>
+//segunda-horario-1
+var elementclass11 = document.getElementById("segunda-h1-m");
+var elementname11 = document.getElementById("segunda-h1-n");
+var elementduration11 = document.getElementById("segunda-h1-h");
+	elementclass11.innerHTML = "Lasers 1";
+	elementname11.innerHTML = "Prof Dr. Joao Kleber";
+	elementduration11.innerHTML = "6h";
+//segunda-horario-2
+var elementclass12 = document.getElementById("segunda-h2-m");
+var elementname12 = document.getElementById("segunda-h2-n");
+var elementduration12 = document.getElementById("segunda-h2-h");
+	elementclass12.innerHTML = "Ultra Violeta";
+	elementname12.innerHTML = "Prof Dr. Faustao Oloco Meu";
+	elementduration12.innerHTML = "4h";
+//segunda-horario-3
+var elementclass13 = document.getElementById("segunda-h3-m");
+var elementname13 = document.getElementById("segunda-h3-n");
+var elementduration13 = document.getElementById("segunda-h3-h");
+	elementclass13.innerHTML = "Ultra Violeta";
+	elementname13.innerHTML = "Prof Dr. Faustao Oloco Meu";
+	elementduration13.innerHTML = "4h";
+//segunda-horario-4
+var elementclass14 = document.getElementById("segunda-h4-m");
+var elementname14 = document.getElementById("segunda-h4-n");
+var elementduration14 = document.getElementById("segunda-h4-h");
+	elementclass14.innerHTML = "Infravermelho";
+	elementname14.innerHTML = "Prof Dr. Gugu Caiu da Escada";
+	elementduration14.innerHTML = "2h";
+</script>
 
 
 
